503dee7b543ea812bcca6364ad31791d4ac3b947
zyreoo <simone.marton89@gmail.com>
2025-11-08 13:44:57 +0200

    add new fonts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,14 +3,44 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+
+
+  final String username = "simone";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Screen')),
+      appBar: AppBar(),
       body: Center(
-        child: Text(
-          'Welcome to the Home Screen',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.blue,
+              alignment: Alignment.center,
+              child: Text("Welcome back ${username[0].toUpperCase() + username.substring(1)}"),
+            ),
+
+
+            Expanded(child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 0.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    color: Colors.red,
+                    height: 200.0,
+                    alignment: Alignment.center,
+                    child: Text("Here is your data analyzed"),
+                  ),
+                  Container( color: Colors.red,
+                    height: 200.0,
+                    alignment: Alignment.center,
+                    child: Text("Here is your data analyzed"),),
+                    
+                ],
+              ),
+            ))
+          ],
         ),
       ),
     );
